a7577283c6e2fbb03d0f6841088ec859568ede2c
srinarayan96 <shrinarayan1996@gmail.com>
2024-09-19 23:49:21 +0530

    second commit

--- src/main/java/com/example/azure_sri/HelloController.java
+++ src/main/java/com/example/azure_sri/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String callHello(){
-        return "Hello World";
+        return "Hello World - first deployment";
     }
 }
