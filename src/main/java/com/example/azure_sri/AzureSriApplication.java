6fef7b0eb0867734153514790710a0238b5ce18c
srinarayan96 <shrinarayan1996@gmail.com>
2024-09-19 23:36:19 +0530

    first commit

---
+++ src/main/java/com/example/azure_sri/AzureSriApplication.java
@@ -0,0 +1,13 @@
+package com.example.azure_sri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureSriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureSriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/azure_sri/AzureSriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azure_sri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureSriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
